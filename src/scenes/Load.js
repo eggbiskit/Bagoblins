65040f1001aee184f593326afeec3029dcf6aaaa
Grace Ho <grho@ucsc.edu>
2022-05-13 16:00:08 -0700

    play button

--- src/scenes/Load.js
+++ src/scenes/Load.js
@@ -8,7 +8,9 @@ class Load extends Phaser.Scene {
        this.load.image("inventory", "assets/sprites/Inventory.png");
        this.load.image("tile", "assets/sprites/slot.png");
        this.load.image("cursor", "assets/sprites/Cursor.png");
-       this.load.image("item", "assets/sprites/blackpotion.png")
+       this.load.image("item", "assets/sprites/blackpotion.png");
+       this.load.image("play_button", "assets/sprites/play_button.png");
+       this.load.image("play_button_pressed", "assets/sprites/play_button_pressed.png");
    }
 
    create() {
