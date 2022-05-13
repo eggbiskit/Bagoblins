ae483fd6e9d8473504fa18821cfbc5a71d497809
Jacob Dickerman <jwdicker@ucsc.edu>
2022-05-13 16:22:36 -0700

    Made play button turn off when mouse is not on it

--- README.md
+++ README.md
@@ -9,5 +9,6 @@ Grace Ho
 ## External Sources
 
 [Breadstick Bonanza](https://github.com/jwdicker/BreadstickBonanza)
+[Rocket Patrol Extensions](https://github.com/jwdicker/RocketPatrolExtensions)
 
 ## External Sound Effects
--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -7,10 +7,11 @@ class Menu extends Phaser.Scene {
         console.log("Menu");
 
         // SRC: https://snowbillr.github.io/blog//2018-07-03-buttons-in-phaser-3/
-        const playButton = this.add.image(100, 100, 'play_button')
-        .setInteractive()
-        .on('pointerdown', () => this.scene.start("play"));
-        playButton.on('pointerover', () => { this.add.image(100, 100, 'play_button_pressed'); });
+        const playButton = this.add.image(100, 100, 'play_button');
+        playButton.setInteractive()
+        playButton.on('pointerdown', () => this.scene.start("play"));
+        playButton.on('pointerover', () => { playButton.setTexture('play_button_pressed'); });
+        playButton.on('pointerout', () => { playButton.setTexture('play_button'); })
 
         //this.scene.start("play");
     }
