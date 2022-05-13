fda49cc93ad3dd5b6ece38ca05e64f27c9095d26
Grace Ho <grho@ucsc.edu>
2022-05-13 16:21:43 -0700

    update button hovering state

--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -9,9 +9,11 @@ class Menu extends Phaser.Scene {
         // SRC: https://snowbillr.github.io/blog//2018-07-03-buttons-in-phaser-3/
         const playButton = this.add.image(100, 100, 'play_button')
         .setInteractive()
-        .on('pointerdown', () => this.scene.start("play"));
-        playButton.on('pointerover', () => { this.add.image(100, 100, 'play_button_pressed'); });
-
-        //this.scene.start("play");
+        // click button
+        .on('pointerdown', () => this.scene.start("play"))
+        // pointer hovering  on button
+        .on('pointerover', () =>  this.add.image(100, 100, 'play_button_pressed'))
+        // pointer not on button
+        .on('pointerout', () =>  this.add.image(100, 100, 'play_button'));
     }
 }
