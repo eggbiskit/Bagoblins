9667804af2b54acac46ab55c8889e874b2011faf
Grace Ho <grho@ucsc.edu>
2022-05-21 00:41:25 -0700

    added another tutorial scene

--- src/scenes/Tutorial.js
+++ src/scenes/Tutorial.js
@@ -22,42 +22,6 @@ class Tutorial extends Phaser.Scene {
         this.add.bitmapText(117, 160, 'pixel_font', 'Z', 5);
         this.add.bitmapText(123, 160, 'pixel_font', 'TO SEND OUT DELIVERIES', 5);
 
-        // arrow keys
-        this.ak = this.add.sprite(160, 75);
-        this.anims.create({
-            key: 'akAni',
-            frames: this.anims.generateFrameNames('tutorial_atlas', {
-                prefix: 'tut_arrowkey',
-                start: 1,
-                end: 4,
-            }),
-            frameRate: 2,
-            repeat: -1
-        });
-        
-        //this.ak.anims.play('akAni');
-        //this.add.bitmapText(130, 105, 'pixel_font', 'ARROW KEYS TO', 5);
-        //this.add.bitmapText(115, 112, 'pixel_font', 'MOVE AROUND INVENTORY', 5); // +7 pix for next line
-
-        // x key
-        this.x = this.add.sprite(160, 135);
-        this.anims.create({
-            key: 'xAni',
-            frames: this.anims.generateFrameNames('tutorial_atlas', {
-                prefix: 'tut_x',
-                start: 1,
-                end: 2 ,
-            }),
-            frameRate: 1 ,
-            repeat: -1
-        });
-        //this.x.anims.play('xAni');
-        //this.add.bitmapText(101, 152, 'pixel_font', 'X TO GRAB AND UNGRAB POTIONS', 5);
-        //this.add.bitmapText(112, 159, 'pixel_font', 'STACK UP TO  +10+  POTIONS', 5);
-
-        this.ak.anims.remove('xAni');
-        this.x.anims.remove('xAni');
-
         // press space to play
         keySpace = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes[gameSettings.keybinds.space]);
         this.play = this.add.sprite(game.config.width / 2, game.config.height / 5 + 150);
@@ -74,14 +38,8 @@ class Tutorial extends Phaser.Scene {
         this.play.anims.play('playAni', true);
 
         keySpace.on("down", () => {
-            this.ak.anims.play('akAni');
-            this.add.bitmapText(130, 105, 'pixel_font', 'ARROW KEYS TO', 5);
-            this.add.bitmapText(115, 112, 'pixel_font', 'MOVE AROUND INVENTORY', 5); // +7 pix for next line
-            this.x.anims.play('xAni');
-            this.add.bitmapText(101, 152, 'pixel_font', 'X TO GRAB AND UNGRAB POTIONS', 5);
-            this.add.bitmapText(112, 159, 'pixel_font', 'STACK UP TO  +10+  POTIONS', 5);
-            this.scene.start("play");
-
+            this.sound.play("temp_sfx");
+            this.scene.start("tutorial2");
         });
     }
 }
