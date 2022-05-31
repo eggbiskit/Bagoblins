3212981767afc8bfd638a98fc042647c923a37d6
Grace Ho <grho@ucsc.edu>
2022-05-31 11:46:38 -0700

    switch tutorial order

--- src/scenes/Tutorial.js
+++ src/scenes/Tutorial.js
@@ -19,16 +19,41 @@ class Tutorial extends Phaser.Scene {
         const clipb = this.add.image(game.config.width / 2, game.config.height / 2 - 4, 'tutorial_atlas', 'clipboard').setScale(2);
         const overview = this.add.image(game.config.width / 2, 40 , 'tutorial_atlas', 'tut_overview').setScale(2); 
 
-        // c key
-        this.add.image(160, 73, 'tutorial_atlas', 'bg_invoice');
-        this.add.image(160, 76, 'tutorial_atlas', 'tut_potion_c');
-        this.add.bitmapText(106, 103, 'pixel_font', 'C TO PUT INVOICE  IN INVENTORY', 5);
+        // arrow keys
+        this.ak = this.add.sprite(160, 75);
+        this.anims.create({
+            key: 'akAni',
+            frames: this.anims.generateFrameNames('tutorial_atlas', {
+            prefix: 'tut_arrowkey',
+            start: 1,
+            end: 4,
+        }),
+            frameRate: 2,
+            repeat: -1
+        });
 
-        // z key
-        this.add.image(160, 137, 'tutorial_atlas', 'bg_orders');
-        this.add.image(161, 134, 'tutorial_atlas', 'tut_potion_z').alpha = 0.5;
-        this.add.bitmapText(117, 165, 'pixel_font', 'Z', 5);
-        this.add.bitmapText(123, 165, 'pixel_font', 'TO SEND OUT ORDERS', 5);
+        this.ak.anims.play('akAni');
+        this.add.bitmapText(130, 105, 'pixel_font', 'ARROW KEYS TO', 5);
+        this.add.bitmapText(115, 112, 'pixel_font', 'MOVE AROUND INVENTORY', 5); // +7 pix for next line
+
+        // x key
+        this.x = this.add.sprite(160, 135);
+        this.anims.create({
+            key: 'xAni',
+            frames: this.anims.generateFrameNames('tutorial_atlas', {
+            prefix: 'tut_x',
+            start: 1,
+            end: 2 ,
+        }),
+            frameRate: 1 ,
+            repeat: -1
+        });
+        this.x.anims.play('xAni');
+        this.add.bitmapText(101, 152, 'pixel_font', 'X TO GRAB AND UNGRAB POTIONS', 5);
+        this.add.bitmapText(112, 159, 'pixel_font', 'STACK UP TO  +10+  POTIONS', 5);
+
+        this.ak.anims.remove('xAni');
+        this.x.anims.remove('xAni');
 
         // press space to play
         keySpace = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes[gameSettings.keybinds.space]);
@@ -36,10 +61,10 @@ class Tutorial extends Phaser.Scene {
         this.anims.create({
             key: 'playAni',
             frames: this.anims.generateFrameNames('menu_atlas', {
-                prefix: 'play',
-                start: 1,
-                end: 2,
-            }),
+            prefix: 'play',
+            start: 1,
+            end: 2,
+        }),
             frameRate: 2,
             repeat: -1
         });
