f414a4f066848b7b91a30ee606dfa082e447c9c4
Jacob Dickerman <jwdicker@ucsc.edu>
2022-05-31 19:08:17 -0700

    Asset Implementation

--- src/scenes/Load.js
+++ src/scenes/Load.js
@@ -37,16 +37,16 @@ class Load extends Phaser.Scene {
         // SFX Loading
         // *temp sfx, replace later*
         this.load.audio("temp_sfx", "assets/SFX/select.mp3");
-        this.load.audio("input_pull", "assets/SFX/select.mp3");
-        this.load.audio("drop_stack", "assets/SFX/potion.mp3");
-        this.load.audio("pick_up_stack", "assets/SFX/potion.mp3");
-        this.load.audio("output_push", "assets/SFX/select.mp3");
+        this.load.audio("input_pull", "assets/SFX/moveItem.wav");
+        this.load.audio("drop_stack", "assets/SFX/drop.wav");
+        this.load.audio("pick_up_stack", "assets/SFX/pickUp.wav");
+        this.load.audio("output_push", "assets/SFX/moveItem.wav");
         this.load.audio("create", "assets/SFX/appear.mp3");
         this.load.audio("request", "assets/SFX/receiving.wav");
-        this.load.audio("wrong", "assets/SFX/select.mp3");
+        this.load.audio("wrong", "assets/SFX/request.mp3");
         this.load.audio("death", "assets/SFX/select.mp3");
         this.load.audio("move", "assets/SFX/move.mp3");
-        this.load.audio("BGM", "assets/music/BagoblinsTheme.wav");
+        this.load.audio("BGM", "assets/music/BagTheme.wav");
 
         // JSON loading
         this.load.json("gameSettings", "src/settings/GameSettings.json");
