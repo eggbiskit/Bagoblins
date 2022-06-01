da171933a2b7da31616759b3afa2e0cb0e9dd0a2
Jacob Dickerman <jwdicker@ucsc.edu>
2022-06-01 15:08:13 -0700

    Adjusted pausing

--- src/main.js
+++ src/main.js
@@ -23,10 +23,10 @@ let endTime;
 let endCause;
 
 // Pause game on tab out
-document.addEventListener("visibilitychange", function() {
-   if(document.visibilityState === "visible") {
-      console.log("unpause");
-   } else {
-      console.log("pause");
-   }
+window.addEventListener("focus", function () {
+   console.log("unpause");
+});
+
+window.addEventListener("blur", function () {
+   console.log("pause");
 });
